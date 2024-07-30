874e1b3a419949a65d47ab9376b23d719e27c119
Ario2mahb <138014505+Ario2mahb@users.noreply.github.com>
2024-07-30 09:24:14 +0330

    Update .eslintrc.js
    Signed-off-by: Ario2mahb <138014505+Ario2mahb@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     page: true,
-    REACT_APP_ENV: true,
+    REACT_APP_ENV: false,
   },
   plugins: ['react-hooks'],
   rules: {
